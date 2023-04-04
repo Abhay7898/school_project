abde5d6a5d670c09680c9e66774985d96ced21fc
Abhay7898 <128139505+Abhay7898@users.noreply.github.com>
2023-04-04 23:23:28 +0530

    new

--- src/model/Department.java
+++ src/model/Department.java
@@ -1,5 +1,25 @@
 package model;
 
 public class Department {
-
+	private int deptid;
+	private String deptname;
+	private String deptlocation;
+	public int getDeptid() {
+		return deptid;
+	}
+	public void setDeptid(int deptid) {
+		this.deptid = deptid;
+	}
+	public String getDeptname() {
+		return deptname;
+	}
+	public void setDeptname(String deptname) {
+		this.deptname = deptname;
+	}
+	public String getDeptlocation() {
+		return deptlocation;
+	}
+	public void setDeptlocation(String deptlocation) {
+		this.deptlocation = deptlocation;
+	}
 }
---
+++ src/model/Staff.java
@@ -0,0 +1,115 @@
+package model;
+
+import java.util.Date;
+
+public class Staff {
+	private int sfid;
+	private String sffname;
+	private String sflastname;
+	private int sfage;
+	private String sfgender;
+	private String sfaddress;
+	private int sfmono;
+	private String sfemailid;
+	private Date sfdoj;
+	private String sfdepartment;
+	private String sfwork;
+	private String sfsalary;
+
+	public int getSfid() {
+		return sfid;
+	}
+
+	public void setSfid(int sfid) {
+		this.sfid = sfid;
+	}
+
+	public String getSffname() {
+		return sffname;
+	}
+
+	public void setSffname(String sffname) {
+		this.sffname = sffname;
+	}
+
+	public String getSflastname() {
+		return sflastname;
+	}
+
+	public void setSflastname(String sflastname) {
+		this.sflastname = sflastname;
+	}
+
+	public int getSfage() {
+		return sfage;
+	}
+
+	public void setSfage(int sfage) {
+		this.sfage = sfage;
+	}
+
+	public String getSfgender() {
+		return sfgender;
+	}
+
+	public void setSfgender(String sfgender) {
+		this.sfgender = sfgender;
+	}
+
+	public String getSfaddress() {
+		return sfaddress;
+	}
+
+	public void setSfaddress(String sfaddress) {
+		this.sfaddress = sfaddress;
+	}
+
+	public int getSfmono() {
+		return sfmono;
+	}
+
+	public void setSfmono(int sfmono) {
+		this.sfmono = sfmono;
+	}
+
+	public String getSfemailid() {
+		return sfemailid;
+	}
+
+	public void setSfemailid(String sfemailid) {
+		this.sfemailid = sfemailid;
+	}
+
+	public Date getSfdoj() {
+		return sfdoj;
+	}
+
+	public void setSfdoj(Date sfdoj) {
+		this.sfdoj = sfdoj;
+	}
+
+	public String getSfdepartment() {
+		return sfdepartment;
+	}
+
+	public void setSfdepartment(String sfdepartment) {
+		this.sfdepartment = sfdepartment;
+	}
+
+	public String getSfwork() {
+		return sfwork;
+	}
+
+	public void setSfwork(String sfwork) {
+		this.sfwork = sfwork;
+	}
+
+	public String getSfsalary() {
+		return sfsalary;
+	}
+
+	public void setSfsalary(String sfsalary) {
+		this.sfsalary = sfsalary;
+	}
+
+}
