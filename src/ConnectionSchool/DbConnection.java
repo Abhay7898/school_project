e75bc2eeebb3171dc225f95f7d22dcfb55481c32
Abhay7898 <128139505+Abhay7898@users.noreply.github.com>
2023-04-05 00:01:02 +0530

    Connection

--- src/ConnectionSchool/DbConnection.java
+++ src/ConnectionSchool/DbConnection.java
@@ -1,18 +1,5 @@
-package ConnectionSchool;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
+package connectionSchool;
 
 public class DbConnection {
-	
-	public static Connection getConnection() {
-		Connection con = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/school?characterEncoding=utf8","root","root");
-		}catch (Exception e) {
-			System.out.println(e);
-		}
-		return con;
-	}
+
 }
